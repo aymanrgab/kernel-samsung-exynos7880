7a57418219e0ced3b9f5688dd79c0afc389c35bd
aymanrgab <aymanrar2c@gmail.com>
2022-05-06 09:53:11 +0200

    add a7 defconfig/rename to halium

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/ayman/Downloads/m/halium/bin/aarch64-linux-android-
+export CROSS_COMPILE=../halium/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=p
 
-make lineage-a5y17lte_defconfig
+make halium-10-a5y17lte_defconfig
 make -j64
 
